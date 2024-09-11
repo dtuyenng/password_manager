3cd51cee0113a6e6da1c90364eca255a22aa494b
dtuyenng <136381756+dtuyenng@users.noreply.github.com>
2024-09-11 14:22:37 -0700

    Commit
    Commit

--- main.py
+++ main.py
@@ -11,10 +11,14 @@ from tkinter import messagebox
 
 TODO:
     - Add copy function
-    - scrollable treeview
-    - make frame size static
+    - scrollable treeview (done)
+    - make frame size static (done)
     - create menu
     - convert and load from json in menu
+    
+Bugs:
+    - When no item is selected (i.e during start up) and user presses edit, we get IndexError
+    - Scrollbar doesn't have little arrows
 
 """
 
@@ -39,96 +43,89 @@ def main():
         if user_input == "q":
             break
 
-
-
 keychain = Keychain()
 
+def center_window(window, width, height):
+    """Center the window on the screen."""
+    # Get the screen width and height
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+
+    # Calculate the position x, y
+    x = (screen_width // 2) - (width // 2)
+    y = (screen_height // 2) - (height // 2)
+
+    # Set the window size and position
+    window.geometry(f"{width}x{height}+{x}+{y}")
+
+
+# def center_pop(popup, width, height):
+#     """Centers the given window on the main window."""
+#     popup.update_idletasks()  # Ensure the window is updated with current size info
+#
+#     # Calculate the position to center the window
+#     x = window.winfo_x() + (window.winfo_width() // 2) - (width // 2)
+#     y = window.winfo_y() + (window.winfo_height() // 2) - (height // 2)
+#
+#     # Set the geometry of the pop-up window
+#     popup.geometry(f"{width}x{height}+{x}+{y}")
+
+
 def edit_key_popup():
-    """Opens a custom pop-up window centered within the main window."""
     popup = tk.Toplevel(window)  # Make the pop-up window a child of the main window
     popup.title("Edit Key")
+    popup.resizable(False, False)
 
     # Set the size of the pop-up window
-    popup_width = 450
-    popup_height = 300
+    width = 350
+    height = 300
 
-    # Get main window size and position
-    main_window_width = window.winfo_width()
-    main_window_height = window.winfo_height()
-    main_window_x = window.winfo_x()
-    main_window_y = window.winfo_y()
-
-    # Calculate position x, y to center the pop-up in the main window
-    position_x = main_window_x + (main_window_width // 2) - (popup_width // 2)
-    position_y = main_window_y + (main_window_height // 2) - (popup_height // 2)
-
-    # Set the geometry of the pop-up window
-    popup.geometry(f"{popup_width}x{popup_height}+{position_x}+{position_y}")
+    # Center the pop-up window
+    center_window(popup, width, height)
 
     # Create a frame inside the pop-up
     popup_frame = tk.Frame(popup)
-    popup_frame.pack()
+    popup_frame.pack(padx=10, pady=10, fill="x")
 
     # Setting up data needed
-    # Get the selected row's Iid
     edited_key = password_table.selection()[0]
-
-    # Get the values of the selected row
-    # item_value returns ('6', 'Google', 'neyuttad@gmail.com', 't1^1Cg78pV%EJhi69m-k')
     item_values = password_table.item(edited_key, 'values')
-    label_variable = item_values[1]
-    username_variable = item_values[2]
-    password_variable = item_values[3]
 
+    popup_label_variable = item_values[1]
+    popup_username_variable = item_values[2]
+    popup_password_variable = item_values[3]
 
     # Add widgets
-    label_popup_label = tk.Label(popup_frame, text="Label")
-    label_popup_label.grid(row=0, column=1, sticky="E")
-    label_entry = ttk.Entry(popup_frame, textvariable=label_variable)
-    label_entry.delete(0, tk.END)  #first delete then insert or it will show double
-    label_entry.insert(0, label_variable)
+    tk.Label(popup_frame, text="Label").grid(row=0, column=0, sticky="E")
+    label_entry = ttk.Entry(popup_frame)
+    label_entry.insert(0, popup_label_variable)
     label_entry.config(state='readonly')
-    label_entry.grid(row=1, column=0, columnspan=2, sticky="WE")
+    label_entry.grid(row=0, column=1, sticky="WE", padx=5, pady=5)
 
-    label_popup_username = tk.Label(popup_frame, text="Username")
-    label_popup_username.grid(row=3, column=0, sticky="E")
-    username_entry = ttk.Entry(popup_frame, textvariable=username_variable)
-    username_entry.delete(0, tk.END)
-    username_entry.insert(0, username_variable)
+    tk.Label(popup_frame, text="Username").grid(row=1, column=0, sticky="E")
+    username_entry = ttk.Entry(popup_frame)
+    username_entry.insert(0, popup_username_variable)
     username_entry.config(state='readonly')
-    username_entry.grid(row=4, column=0, sticky="E")
+    username_entry.grid(row=1, column=1, sticky="WE", padx=5, pady=5)
 
-    label_popup_password = tk.Label(popup_frame, text="Password")
-    label_popup_password.grid(row=3, column=1, sticky="E")
-    password_entry = ttk.Entry(popup_frame, textvariable=password_variable)
-    password_entry.delete(0, tk.END)
-    password_entry.insert(0, password_variable)
+    tk.Label(popup_frame, text="Password").grid(row=2, column=0, sticky="E")
+    password_entry = ttk.Entry(popup_frame)
+    password_entry.insert(0, popup_password_variable)
     password_entry.config(state='readonly')
-    password_entry.grid(row=4, column=1, sticky="E")
-
+    password_entry.grid(row=2, column=1, sticky="WE", padx=5, pady=5)
 
 
 def add_key_popup():
-    """Opens a custom pop-up window centered within the main window."""
     popup = tk.Toplevel(window)  # Make the pop-up window a child of the main window
     popup.title("Add Key")
+    popup.resizable(False, False)
 
     # Set the size of the pop-up window
-    popup_width = 450
-    popup_height = 300
+    width = 450
+    height = 200
 
-    # Get main window size and position
-    main_window_width = window.winfo_width()
-    main_window_height = window.winfo_height()
-    main_window_x = window.winfo_x()
-    main_window_y = window.winfo_y()
-
-    # Calculate position x, y to center the pop-up in the main window
-    position_x = main_window_x + (main_window_width // 2) - (popup_width // 2)
-    position_y = main_window_y + (main_window_height // 2) - (popup_height // 2)
-
-    # Set the geometry of the pop-up window
-    popup.geometry(f"{popup_width}x{popup_height}+{position_x}+{position_y}")
+    # Centering window function
+    center_window(popup, width, height)
 
     # Create a frame inside the pop-up
     popup_frame = tk.Frame(popup)
@@ -157,9 +154,9 @@ def add_key_popup():
 
     # Add buttons
     add_button_popup = ttk.Button(popup_frame, text="Add Key", command= lambda: add_key_button_event(popup))
-    add_button_popup.grid(row=6, column=0, sticky="WE")
+    add_button_popup.grid(row=6, column=0,  sticky="WE", pady=20)
     cancel_button = ttk.Button(popup_frame, text="Cancel", command=popup.destroy)
-    cancel_button.grid(row=6, column=1, sticky="WE")
+    cancel_button.grid(row=6, column=1, sticky="WE", pady=20)
     generate_password_button = ttk.Button(popup_frame, text="Generate Random", width=2, command=lambda: password_variable.set(password_generator(20)))
     generate_password_button.grid(row=5, column=1, sticky="NSEW")
 
@@ -202,21 +199,38 @@ def print_keychain():
 
 window = tk.Tk()
 window.title("My Password Manager")
-window.geometry("800x500")
 
-password_table = ttk.Treeview(window, columns=("id", "label", "username", "password"), show="headings")
+center_window(window, 800, 500)
+# window.resizable(False, False)
+
+
+main_frame = tk.Frame(window)
+main_frame.pack(pady=20)
+
+#Scroll bar for the TreeView
+scroll_bar = tk.Scrollbar(main_frame, orient="vertical")
+scroll_bar.grid(row=0, column=1, sticky="NS")
+
+password_table = ttk.Treeview(main_frame, yscrollcommand= scroll_bar.set ,
+                              # columns=("id", "label", "username", "password"),
+                              columns=("id", "label", "username", "password"),
+                              show="headings", height=5)
+password_table.grid(row=0, column=0, sticky="NSEW")
+
+scroll_bar.config(command=password_table.yview)
+
 password_table.heading("id", text="id")
 password_table.heading("label", text="Label")
 password_table.heading("username", text="username")
 password_table.heading("password", text="password")
-password_table.pack()
+
 
 #column widths
-
-password_table.column("id", width=30)
+password_table.column("id", width=0, stretch=False) #hide column from user
 password_table.column("label", width=230)
 password_table.column("username", width=250)
 password_table.column("password", width=250)
+
 
 # App Variables
 label_variable = tk.StringVar()
